a1412aa159323b457d673285c76ee39c4a15d5f7
JonLuca DeCaro <jdecaro@usc.edu>
2018-04-15 15:06:35 -0700

    pm2flag

--- pm2.config.js
+++ pm2.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-  /**
-   * Application configuration section
-   * http://pm2.keymetrics.io/docs/usage/application-declaration/
-   */
-  apps: [// First application
-    {
-      name: 'blog',
-      script: 'start.sh',
-    }
+    /**
+     * Application configuration section
+     * http://pm2.keymetrics.io/docs/usage/application-declaration/
+     */
+    apps: [ // First application
+        {
+            name: 'blog',
+            script: 'start.sh',
+            args: '--no-autorestart'
+        }
 
-  ]
+    ]
 };
