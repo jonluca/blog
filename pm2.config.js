ac792b2900a7de3057991fd5a867a209c6809646
JonLuca DeCaro <jdecaro@usc.edu>
2018-09-30 16:39:37 -0400

    Formatted code, added share functionality
    Former-commit-id: 4ffaef185057686d69dc3530d47c14388fff61ea

--- pm2.config.js
+++ pm2.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    /**
-     * Application configuration section
-     * http://pm2.keymetrics.io/docs/usage/application-declaration/
-     */
-    apps: [ // First application
-        {
-            name: 'blog',
-            script: 'start.sh',
-            args: '--no-autorestart',
-            exec_interpreter: "bash",
-            exec_mode: "fork_mode",
-            autorestart: false
-        }
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [ // First application
+    {
+      name: 'blog',
+      script: 'start.sh',
+      args: '--no-autorestart',
+      exec_interpreter: "bash",
+      exec_mode: "fork_mode",
+      autorestart: false
+    }
 
-    ]
+  ]
 };
