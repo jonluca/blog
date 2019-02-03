237f9008a7cd4082fd9f9bd2cb79f831551336db
JonLuca DeCaro <jdecaro@usc.edu>
2019-02-02 19:43:24 -0800

    'removed scripts, added images'
    Former-commit-id: 71d7032e7e4651b5d8d43ce576af9192919433da

--- dig.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-query=""
-for type in {A,AAAA,ALIAS,CNAME,MX,NS,PTR,SOA,SRV,TXT,DNSKEY,DS,NSEC,NSEC3,NSEC3PARAM,RRSIG,AFSDB,ATMA,CAA,CERT,DHCID,DNAME,HINFO,ISDN,LOC,MB,MG,MINFO,MR,NAPTR,NSAP,RP,RT,TLSA,X25};
-do
-	query="$query $type $1" 
-done
-dig +noall +short +noshort +answer $query 2>/dev/null
