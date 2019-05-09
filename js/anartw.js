ff6ae25f8ece689c276700a200e2b19735afd233
JonLuca DeCaro <jdecaro@usc.edu>
2019-05-09 17:07:48 +0900

    'Added more images, carousel'
    Former-commit-id: 25099fbd079aa27812818600deefbc25a786d126

---
+++ js/anartw.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.tokyo-slideshow').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        adaptiveHeight: true,
+        cssEase: 'linear'
+    });
+});
