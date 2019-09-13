9c6799afd33a681f7a2ae4ce5b6aebb6edf3b19a
JonLuca DeCaro <jdecaro@pinterest.com>
2019-09-13 15:35:28 -0700

    script
    Former-commit-id: 69bf9496ba85fcaaebcb023e1448050511443076

--- start.sh
+++ start.sh
@@ -3,13 +3,14 @@ echo_time() {
 }
 # Build bundle first to reflect changes, then build in production to minify
 echo_time "Building"
-JEKYLL_ENV="production" bundle exec jekyll build
+JEKYLL_ENV="production" bundle exec jekyll build --verbose --trace
 # Run minifiers
 echo_time "Minifying CSS"
-find . -iname "*.css" -exec yui-compressor '{}' > '{}' \;
+find . -iname "*.css" -exec yui-compressor '{}' -o '{}' \; -exec echo {} done \;
 echo_time "Minifying js"
-find . -iname "*.js" -exec yui-compressor '{}' > '{}' \;
+find . -iname "*.js" -exec yui-compressor '{}' -o '{}' \; -exec echo {} done \;
 echo_time "Minifying HTML"
-find . -iname "*.html" -exec html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true '{}' -o '{}' \; -exec echo {} done \;# compress
+find . -iname "*.html" -exec html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true '{}' -o '{}' \; -exec echo {} done \;
+# compress
 echo_time "Compressing"
 find . -type f -exec zopfli --i50 '{}' \; -exec echo {} done \;
