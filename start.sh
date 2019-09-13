bfb50f9bb14a81075aae85322950fee4ee5270e4
JonLuca DeCaro <jdecaro@pinterest.com>
2019-09-13 15:27:03 -0700

    Updated script

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo_time() {
-    date +"%R $*"
+    echo $(date --iso-8601=seconds) "$*"
 }
 # Build bundle first to reflect changes, then build in production to minify
 echo_time "Building"
@@ -12,4 +12,4 @@ find . -iname "*.js" -exec yui-compressor '{}' > '{}' \;
 echo_time "Minifying HTML"
 find . -iname "*.html" -exec html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true '{}' -o '{}' \; -exec echo {} done \;# compress
 echo_time "Compressing"
-find . -type f -exec zopfli --i100 '{}' \; -exec echo {} done \;
+find . -type f -exec zopfli --i50 '{}' \; -exec echo {} done \;
