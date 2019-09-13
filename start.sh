b12ba0c6af1c602ffc1a58fefe4ebda839daf731
JonLuca DeCaro <jdecaro@pinterest.com>
2019-09-13 15:24:23 -0700

    Updated script and compression algorithm

--- start.sh
+++ start.sh
@@ -1,3 +1,15 @@
-bundle exec jekyll build
+echo_time() {
+    date +"%R $*"
+}
 # Build bundle first to reflect changes, then build in production to minify
+echo_time "Building"
 JEKYLL_ENV="production" bundle exec jekyll build
+# Run minifiers
+echo_time "Minifying CSS"
+find . -iname "*.css" -exec yui-compressor '{}' > '{}' \;
+echo_time "Minifying js"
+find . -iname "*.js" -exec yui-compressor '{}' > '{}' \;
+echo_time "Minifying HTML"
+find . -iname "*.html" -exec html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true '{}' -o '{}' \; -exec echo {} done \;# compress
+echo_time "Compressing"
+find . -type f -exec zopfli --i100 '{}' \; -exec echo {} done \;
