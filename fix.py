c3e9fb2cbd6e76b33bbdf0fcc751bca1f5d32d45
JonLuca DeCaro <jdecaro@pinterest.com>
2020-02-09 20:08:19 -0800

    removedfix

--- fix.py
+++
@@ -1,7 +0,0 @@
-from unidecode import unidecode
-iscontents = open('interesting-snippets.md').read()
-
-iscontents = unidecode(iscontents)
-
-with open('interesting-snippets.md','w') as out:
-	out.write(iscontents)
