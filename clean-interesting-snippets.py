449186df8fe2c4e65e2148c3e46633fac9dcc156
JonLuca DeCaro <jdecaro@usc.edu>
2020-02-19 08:02:20 -0800

    clean

--- clean-interesting-snippets.py
+++ clean-interesting-snippets.py
@@ -1,7 +1,13 @@
 from unidecode import unidecode
-int_snip = open('interesting-snippets.md').read()
-contents = unidecode(int_snip)
+import re
 
+int_snip = open('interesting-snippets.md').read()
+# convert all unicode to closest ascii equivalent
+contents = unidecode(int_snip)
+# replace double dash with single
+contents = contents.replace(r'(?<!\-)--','-')
+# replace double space with single
+contents = re.sub(' +', ' ', contents)
 with open('interesting-snippets.md','w') as out:
 	out.write(contents)
 	out.close()
