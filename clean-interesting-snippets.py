c8a870367dcfdafbb8bd4be2f486ca2b508d77f5
JonLuca DeCaro <jdecaro@usc.edu>
2020-02-19 08:06:30 -0800

    script and interesting snippet entry
    Former-commit-id: d7f0d41bfcd6dd7986d8ee044c229b88bff82887

--- clean-interesting-snippets.py
+++ clean-interesting-snippets.py
@@ -4,8 +4,8 @@ import re
 int_snip = open('interesting-snippets.md').read()
 # convert all unicode to closest ascii equivalent
 contents = unidecode(int_snip)
-# replace double dash with single
-contents = contents.replace(r'(?<!\-)--','-')
+# replace double dash with single, but don't match triplets for jekyll
+contents = re.sub(r'(?<!-)--(?!-)','-', contents)
 # replace double space with single
 contents = re.sub(' +', ' ', contents)
 with open('interesting-snippets.md','w') as out:
