66cf13b7d269cbd06204f8bb8369159d9f253a67
JonLuca DeCaro <jdecaro@usc.edu>
2020-09-06 22:07:54 -0700

    clean

--- clean-interesting-snippets.py
+++ clean-interesting-snippets.py
@@ -1,13 +1,14 @@
 from unidecode import unidecode
 import re
 
-int_snip = open('interesting-snippets.md').read()
+path = 'interesting-snippets.md'
+int_snip = open(path).read()
 # convert all unicode to closest ascii equivalent
 contents = unidecode(int_snip)
 # replace double dash with single, but don't match triplets for jekyll
 contents = re.sub(r'(?<!-)--(?!-)','-', contents)
 # replace double space with single
 contents = re.sub(' +', ' ', contents)
-with open('interesting-snippets.md','w') as out:
+with open(path,'w') as out:
 	out.write(contents)
 	out.close()
