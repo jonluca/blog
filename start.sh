f0e12725b0fd9c990542a07587385f1e3730bbb3
JonLuca DeCaro <jonluca.decaro@gmail.com>
2022-01-25 20:27:18 -0500

    lockfiles

--- start.sh
+++ start.sh
@@ -5,10 +5,3 @@ echo_time() {
 echo_time "Building"
 source ~/.rvm/scripts/rvm
 JEKYLL_ENV="production" bundle exec jekyll build --verbose --trace
-# Run minifiers
-echo_time "Minifying CSS"
-find './_site/' -iname "*.css" -exec yui-compressor '{}' -o '{}' \; -exec echo {} done \;
-echo_time "Minifying js"
-# find . -iname "*.js" -exec yui-compressor '{}' -o '{}' \; -exec echo {} done \;
-echo_time "Minifying HTML"
-find './_site/' -iname "*.html" -exec html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true '{}' -o '{}' \; -exec echo {} done \;
