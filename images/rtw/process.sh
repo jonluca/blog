cb98ce9aa7b13da684bed0f16c583a281bfa5852
JonLuca DeCaro <jonluca.decaro@gmail.com>
2022-05-29 17:46:53 -0400

    remove images history

---
+++ images/rtw/process.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo "Converting png to webp"
+for file in *.{png,jpg}
+do
+	name=$(echo "$file" | cut -f 1 -d '.')'.webp'
+	if [ ! -f "$name" ] || [[ $* == *--force* ]]; then
+    	cwebp -q 75 -alpha_q 10 $file -o $name
+	fi
+done
+
+echo "Done!"
