bec2c58d0d8d8b8a55702cbaeb7d66844c208ff3
JonLuca DeCaro <jonluca.decaro@gmail.com>
2023-06-28 14:26:21 -0400

    change hook to json

--- pm2.config.js
+++ pm2.config.js
@@ -4,7 +4,6 @@ module.exports = {
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
   apps: [
-    // First application
     {
       name: "blog",
       script: "start.sh",
