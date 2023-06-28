db983104053bd27a08110f5fd87528d773d1421a
JonLuca DeCaro <jonluca.decaro@gmail.com>
2023-06-28 14:11:05 -0400

    add bundle to script

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@ echo_time() {
 # Build bundle first to reflect changes, then build in production to minify
 echo_time "Building"
 test -f ~/.rvm/scripts/rvm && source ~/.rvm/scripts/rvm
+bundle
 JEKYLL_ENV="production" bundle exec jekyll build --verbose --trace
 echo_time "Done"
