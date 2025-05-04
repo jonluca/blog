4767459c49cd645ee4b12eb2c3de22d46ae5b88f
JonLuca DeCaro <jonluca.decaro@gmail.com>
2025-05-04 12:06:26 -0700

    bring back rss

---
+++ app/rss/route.ts
@@ -0,0 +1,41 @@
+export const dynamic = "force-static";
+import { getBlogPosts } from "app/posts/utils";
+import { baseUrl } from "../sitemap";
+
+export async function GET() {
+  const allBlogs = await getBlogPosts();
+
+  const itemsXml = allBlogs
+    .sort((a, b) => {
+      if (new Date(a.metadata.date) > new Date(b.metadata.date)) {
+        return -1;
+      }
+      return 1;
+    })
+    .map(
+      (post) =>
+        `<item>
+          <title>${post.metadata.title}</title>
+          <link>${baseUrl}/blog/${post.slug}</link>
+          <description>${post.metadata.summary || ""}</description>
+          <pubDate>${new Date(post.metadata.date).toUTCString()}</pubDate>
+        </item>`,
+    )
+    .join("\n");
+
+  const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
+  <rss version="2.0">
+    <channel>
+        <title>JonLuca's Blog</title>
+        <link>${baseUrl}</link>
+        <description>This is my blog RSS feed</description>
+        ${itemsXml}
+    </channel>
+  </rss>`;
+
+  return new Response(rssFeed, {
+    headers: {
+      "Content-Type": "text/xml",
+    },
+  });
+}
